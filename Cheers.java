59ff2aa2fd5a2735a0b3789296db8f95fa9af67c
shaked cohen <shakedcohen@shakeds-MacBook-Air.local>
2025-11-11 19:26:49 +0200

    hw 2 first session

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,31 @@
-//feedback
 
-/*
-Great work boaz!
-*/
-
-
-// Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	int times = Integer.parseInt("" + args[1]);
+        String s = args[0];
+        String upperS = s.toUpperCase();
+
+        int i=0;
+        String special = "AEFHILMNORSX";
+        
+        int length= upperS.length();
+        while (i<length) { 
+            char letter= upperS.charAt(i);
+            if(special.indexOf(letter)!=-1)
+                System.out.println("Give me an "+letter+": "+letter+"!");
+             else   
+                System.out.println("Give me a "+letter+": "+letter+"!");
+
+            i++;
+        }
+        System.out.println("What does that spell?");
+        int j=0;
+        while (j<times) { 
+           System.out.println(upperS+"!!!"); 
+           j++;
+        }
+
+
+
         }
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,16 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
-    }
+	int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+	int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+	int minutesToAdd = Integer.parseInt("" + args[1]);
+	int TotalMinutes = (hours*60) + minutes + minutesToAdd ;
+
+	int Total_hours = TotalMinutes / 60 ;
+	int New_hours = Total_hours % 24 ; 
+	int New_minutes = TotalMinutes - (Total_hours * 60) ;
+	System.out.print(New_hours<10 ? "0"+New_hours : New_hours);	
+	System.out.println(":"+(New_minutes<10 ? "0"+New_minutes: New_minutes)); 
+
+
+	}
 }
